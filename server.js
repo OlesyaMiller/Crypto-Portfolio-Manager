17367c6fc821f1dd63c38a520bb8cf99a0fdf610
Olesya Miller <olesyam.miller@gmail.com>
2021-08-17 18:41:30 -0400

    finished setting up connection to MongoDB

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get('/', (req, res) => res.json({ msg: "Welcome to the Crypto Portfolio Manager API" }));
+// Connect DB
+connectDB();
+
+app.get('/', (req, res) => 
+    res.json({ msg: "Welcome to the Crypto Portfolio Manager API" })
+);
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
