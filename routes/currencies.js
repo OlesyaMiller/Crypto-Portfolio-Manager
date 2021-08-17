a4cc0ea1ecf76ebdfcd0f8ea24487c737394acc9
Olesya Miller <olesyam.miller@gmail.com>
2021-08-17 18:23:25 -0400

    finished setting up the routes

--- routes/currencies.js
+++ routes/currencies.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
-// @route   POST    api/users
-// @desc    Pegister a user
-// @accsess Public
+// @route   GET    api/currencies
+// @desc    Get all currencies
+// @accsess Private
+router.get('/', (req, res) => {
+    res.send('Get all contacts');
+});
+
+// @route   POST    api/currencies
+// @desc    Add new currency
+// @accsess Private
 router.post('/', (req, res) => {
-    res.send('Register a user');
+    res.send('Add currency');
+});
+
+// @route   PUT    api/currencies/:id
+// @desc    Update currency
+// @accsess Private
+router.put('/:id', (req, res) => {
+    res.send('Update currency');
+});
+
+// @route   DELETE    api/currencies/:id
+// @desc    Delete currency
+// @accsess Private
+router.delete('/', (req, res) => {
+    res.send('Delete currency');
 });
 
 module.exports = router;
