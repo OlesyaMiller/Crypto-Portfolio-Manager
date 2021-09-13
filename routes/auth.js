c36bdf64c54a584504f836ab9f9f83ebcab76a0a
Olesya Miller <olesyam.miller@gmail.com>
2021-09-13 19:01:26 -0400

    enabled jsw authentication

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../middleware/auth');
 const { body, validationResult } = require('express-validator');
 
 const User = require('../models/User');
@@ -10,8 +11,14 @@ const User = require('../models/User');
 // @route   GET    api/auth
 // @desc    Get logged in user
 // @accsess Private
-router.get('/', (req, res) => {
-    res.send('Get logged in user');
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password') // sending data except for password
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 // @route   POST    api/auth
@@ -19,17 +26,17 @@ router.get('/', (req, res) => {
 // @accsess Public
 router.post('/', [
     body('email', 'Please include valid email').isEmail(),
-    body('passowrd', 'Password is required').exists()
+    body('password', 'Password is required').exists()
 ], async (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
 
-    const { isEmail, password } = req.body;
+    const { email, password } = req.body;
 
     try {
-        let user = await User.findOne({ email });
+        let user = await User.findOne({ email }); // ES6 syntax = email: email
 
         if(!user) {
             return res.status(400).json({ msg: 'Invalid Credentials'});
